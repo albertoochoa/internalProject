5e4e9b163a5edbbe40d7b05e40fed546c363e042
GenericDevGeorgia <genericdevgeorgia@GenericDevGeorgias-MacBook-Air.local>
2025-02-04 09:59:02 -0500

    solved getRoute

--- handlers/getHolidaysRoute.js
+++ handlers/getHolidaysRoute.js
@@ -1,23 +1,22 @@
-const HolidaySchema = require('../models/metrics');
+const Holiday = require('../models/metrics'); 
 
 const getPageHolidaysHandler = async (request, h) => {
-    try {
-        const today = new Date();
-        const allHolidays = await HolidaySchema.find({}); 
+  try {
+    const today = new Date();
+    const allHolidays = await Holiday.find({});
+    const upcomingHolidays = allHolidays.filter(holiday => {
+      const holidayDate = new Date(holiday.date);
+      return holidayDate > today;
+    });
 
-        const upcomingHolidays = allHolidays.filter(holiday => { 
-            const holidayDate = new Date(holiday.Date); 
-            return holidayDate > today; 
-        });
-
-        return upcomingHolidays;    
-    } catch (error) {
-        console.error(error);
-        return h.response({ error: 'Failed to fetch holidays' }).code(500);
-    }
+    return upcomingHolidays;
+  } catch (error) {
+    console.error(error);
+    return h.response({ error: 'Failed to fetch holidays' }).code(500);
+  }
 };
 
 module.exports = {
-    method: 'GET',
-    handler: getPageHolidaysHandler
+  method: 'GET',
+  handler: getPageHolidaysHandler
 };
