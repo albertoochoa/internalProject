336fd844c4a516b6850c37a0554b7e18eadce5e9
Alberto Ochoa Galvez <albertoochoagalvez@Albertos-MacBook-Air.local>
2025-02-11 15:33:18 -0500

    tired

--- config/counlty.js
+++ config/counlty.js
@@ -1,11 +1,13 @@
 const Countly = require('countly-sdk-nodejs');
 const dotenv = require('dotenv').config();
+const path = require('path');
+
 
 async function initializeCountly() {
     try {
         Countly.init({
             app_key: process.env.COUNTLY_APP_KEY,  
-            server_url: process.comfig.COUNTLY_SERVER_URL, 
+            server_url: process.env.COUNTLY_SERVER_URL, 
             storage_path: path.join(__dirname, '../countly_storage')  
         });
 
