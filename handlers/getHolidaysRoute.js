d33a67f178c80d2a9cca227b6cbacfe759d64e51
Alberto Ochoa Galvez <albertoochoagalvez@Albertos-MacBook-Air.local>
2025-02-11 13:34:18 -0500

    hey

--- handlers/getHolidaysRoute.js
+++ handlers/getHolidaysRoute.js
@@ -1,43 +1,117 @@
-//const admin = require('firebase-admin'); 
-//const { logEvent } = require('../config/analyticsUtils.js'); 
-const Holidays = require('../models/metrics');
-//const 
+const Holiday = require('../models/metrics');
+const countly = require('countly-sdk-nodejs'); 
 
-const getHolidays = async (req, res) => {
+countly.init({
+  app_key: 'YOUR_APP_KEY', 
+  url: 'https://your_domain_or_server_ip' 
+});
+
+const getPageHolidaysHandler = async (request, h) => {
   try {
     const today = new Date();
-    const allHolidays = await Holidays.find({}); 
+    const allHolidays = await Holiday.find({});
     const upcomingHolidays = allHolidays.filter(holiday => {
       const holidayDate = new Date(holiday.date);
       return holidayDate > today;
     });
 
-    const userId = req.user ? req.user.id : 'anonymous'; 
-    const country = req.query.country || 'unknown'; 
-    const numHolidays = upcomingHolidays.length; 
-
-    admin.analytics().logEvent('holiday_retrieved', {
-      user_id: userId,
-      country: country,
-      number_of_holidays: numHolidays
+    countly.track_event({
+      key: 'get_holidays', 
+      count: 1, 
+      segments: {
+        num_holidays: upcomingHolidays.length, 
+        request_ip: request.info.remoteAddress, 
+      },
     });
 
-    res.status(200).json(upcomingHolidays);
-
+    return upcomingHolidays;
   } catch (error) {
-    console.error("Error getting holidays:", error);
-    await logEvent('get_holidays_error', {
-      error_message: error.message,
+    console.error(error);
+
+    countly.track_event({
+      key: 'get_holidays_error',
+      count: 1,
+      segments: {
+        error_message: error.message,
+      }
     });
 
-    res.status(500).json({ message: 'Error fetching holidays' });
+    return h.response({ error: 'Failed to fetch holidays' }).code(500);
   }
 };
 
 module.exports = {
-    method: 'GET',
-    handler: getHolidays
+  method: 'GET',
+  handler: getPageHolidaysHandler
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const admin = require('firebase-admin'); 
+// //const { logEvent } = require('../config/analyticsUtils.js'); 
+// const Holidays = require('../models/metrics');
+// //const 
+
+// const getHolidays = async (req, res) => {
+//   try {
+//     const today = new Date();
+//     const allHolidays = await Holidays.find({}); 
+//     const upcomingHolidays = allHolidays.filter(holiday => {
+//       const holidayDate = new Date(holiday.date);
+//       return holidayDate > today;
+//     });
+
+//     const userId = req.user ? req.user.id : 'anonymous'; 
+//     const country = req.query.country || 'unknown'; 
+//     const numHolidays = upcomingHolidays.length; 
+
+//     admin.analytics().logEvent('holiday_retrieved', {
+//       user_id: userId,
+//       country: country,
+//       number_of_holidays: numHolidays
+//     });
+
+//     res.status(200).json(upcomingHolidays);
+
+//   } catch (error) {
+//     console.error("Error getting holidays:", error);
+//     await logEvent('get_holidays_error', {
+//       error_message: error.message,
+//     });
+
+//     res.status(500).json({ message: 'Error fetching holidays' });
+//   }
+// };
+
+// module.exports = {
+//     method: 'GET',
+//     handler: getHolidays
+// };
 
 
 
